d25042cefcb397b392d6b0b1c32dd461dd3f2ffe
Admin <mohab.sarhan17@gmail.com>
2023-05-19 06:03:06 +0300

    final app

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,9 @@
 import 'package:flutter/material.dart';
-import 'package:solid_test_app/features/home/presentation/view/home_screen.dart';
+import 'package:solid_test_app/my_app.dart';
 
 void main() {
   runApp(const MyApp());
 }
 
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
 
-  @override
-  Widget build(BuildContext context) {
-    return const MaterialApp(
-      debugShowCheckedModeBanner: false,
-      title: 'Flutter Demo',
-      home: HomeScreen(),
-    );
-  }
-}
 
